8199d21940718e31d397c9a06ab9537eaf9f361c
rashid <momras201@gmail.com>
2023-01-10 03:01:13 +0200

    development env setup

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
 
-export default function App() {
-  console.log("App starting");
+const App = () => {
+  console.log('App starting');
   return (
     <View style={styles.container}>
       <Text>This is just for testing!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
+
+export default App;
