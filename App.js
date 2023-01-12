2a90076aa7167c92b9d6d51b9c296ce9662eb0c2
rashid <momras201@gmail.com>
2023-01-13 01:18:22 +0200

    Navigator

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import Navigator from './navigators/Navigator';
 
 const App = () => {
-  console.log('App starting');
   return (
-    <View style={styles.container}>
-      <Text>This is just for testing!</Text>
+    <>
+      <Navigator></Navigator>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
